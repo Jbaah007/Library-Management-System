c82644e66051a7221ba86997cd73e8d89937ad37
James Baah <167444997+Jbaah007@users.noreply.github.com>
2024-04-19 15:05:38 -0700

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 LIBRARY MANAGEMENT SYSTEM |<a href="google.com" target="_blank"  > Designed by : OWUSU & ASANTE </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
